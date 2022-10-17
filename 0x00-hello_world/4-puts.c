dfc1798c2b6f4453bdb17506aa988b4b908bbb7d
“cynthiaah” <>
2022-10-17 08:31:26 -0700

    size 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - to print a line of code using puts function
+ * main -  print a line of code using puts function
  *
  * return: 0
 */
@@ -8,5 +8,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return 0;
 }
